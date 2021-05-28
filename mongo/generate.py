c0a8e8219674978e69e26866faa40c6ae1096461
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-05-28 16:42:01 +0200

    Changes on Mongo side:
    * dockerize everything
    * automate saving to MongoDB
    * fix errors on generators
    * add few new classes

--- mongo/generate.py
+++ mongo/generate.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field, asdict
 from collections import OrderedDict
 from typing import List, Optional
 from enum import Enum
-import json
+
 
 Faker.seed(1)
 student_faker = Faker(OrderedDict([
@@ -19,11 +19,13 @@ def f(x):
 
 
 random_id = lambda: it_faker.random_int(min=0, max=4000000)
-image_url = lambda: it_faker.lexify(f"{BASE_URL}??????????????????.png")
+url = lambda extension: it_faker.lexify(f"{BASE_URL}??????????????????.{extension}")
+image_url = lambda: url("jpg")
+audio_url = lambda: url("mp4")
 
 
 def lst(factory_, min=0, max=20):
-    return [factory_() for _ in range(it_faker.random_int(min=min, max=max))]
+    return lambda: [factory_() for _ in range(it_faker.random_int(min=min, max=max))]
 
 
 @dataclass
@@ -34,13 +36,20 @@ class Skill:
 
 all_skills = [Skill() for _ in range(200)]
 
+from datetime import datetime, date
+
+
+def date_to_datetime(date: date):
+    return datetime(year=date.year, month=date.month, day=date.day)
+
 
 @dataclass
 class PersonalInfo:
     cf: str = f(it_faker.ssn)
     name: str = f(student_faker.name)
     gender: str = f(lambda: it_faker.random_element(elements=('Male', 'Female', 'Other')))
-    birthday: Optional[str] = f(lambda: it_faker.date_between(start_date="-50y", end_date="-18y"))
+    birthday: Optional[datetime] = f(
+        lambda: date_to_datetime(it_faker.date_between(start_date="-50y", end_date="-18y")))
 
 
 @dataclass
@@ -54,27 +63,24 @@ all_subjects = [Subject() for _ in range(50)]
 
 #### EXERCISES
 
-class ExerciseType(Enum):
-    MULTIPLE_CHOICE = "multipleChoice"
-    IMAGE_QUESTION = "imageQuestion"
-    IMAGE_CHOICE = "imageChoice"
-    HANDWRITING = "handwriting"
-    SPEAKING = "speaking"
-    FREE_TEXT = "freeText"
-    LISTENING = "listening"
+MULTIPLE_CHOICE = "multipleChoice"
+IMAGE_QUESTION = "imageQuestion"
+IMAGE_CHOICE = "imageChoice"
+HANDWRITING = "handwriting"
+SPEAKING = "speaking"
+FREE_TEXT = "freeText"
+LISTENING = "listening"
 
 
 @dataclass
 class BaseExercise:
-    exType: ExerciseType
     question: str = f(lambda: it_faker.sentence())
 
 
 @dataclass
 class MultipleChoiceExercise(BaseExercise):
-    exType = ExerciseType.MULTIPLE_CHOICE
-    options: List[str] = f(
-        lambda: [it_faker.sentence() for _ in range(start=0, stop=it_faker.random_int(min=4, max=6))])
+    exType = MULTIPLE_CHOICE
+    options: List[str] = f(lst(it_faker.sentence, 4, 6))
     correctIndex: int = f(lambda: it_faker.random_int(min=0, max=4))
 
 
@@ -83,51 +89,93 @@ BASE_URL = "https://cdn.sideducation.it/"
 
 @dataclass
 class ImageQuestionExercise(MultipleChoiceExercise):
-    exType = ExerciseType.IMAGE_QUESTION
+    exType = IMAGE_QUESTION
     imageUrl: str = f(image_url)
 
 
 @dataclass
 class ImageChoiceExercise(BaseExercise):
-    exType = ExerciseType.IMAGE_CHOICE
+    exType = IMAGE_CHOICE
     optionUrls: List[str] = f(lst(image_url, min=4, max=9))
     correctIndex: int = f(lambda: it_faker.random_int(min=0, max=4))
 
 
 @dataclass
 class HandWritingExercise(BaseExercise):
-    exType = ExerciseType.HANDWRITING
-    correctAnswer: str = f(it_faker.word())
+    exType = HANDWRITING
+    correctAnswer: str = f(lambda: it_faker.word())
     samples: List[str] = f(lst(image_url, min=2, max=4))
+
 
 @dataclass
 class SpeakingExercise(BaseExercise):
-    exType = ExerciseType.SPEAKING
-
+    exType = SPEAKING
+    correctAnswer: str = f(lambda: it_faker.sentence())
+    sample: str = f(audio_url)
 
 
 @dataclass
 class Exercise:
-    difficulty: str = f(lambda: it_faker.random_choice(elements=("Easy", "Medium", "Hard")))
-    subject: Subject = f(lambda: it_faker.random_choice(elements=all_subjects))
+    difficulty: str = f(lambda: it_faker.random_element(elements=("Easy", "Medium", "Hard")))
+    subject: Subject = f(lambda: it_faker.random_element(elements=all_subjects))
+    content: BaseExercise = f(lambda: it_faker.random_element(elements=(
+        MultipleChoiceExercise, ImageQuestionExercise, ImageChoiceExercise, HandWritingExercise, SpeakingExercise))())
 
 
-# Evaluations and study plan assignments are same they exist on student
+@dataclass
+class Solves:
+    exercise: Exercise = f(Exercise)
+    score: int = f(lambda: it_faker.random_int(0, 100))
+    feedback: List[str] = f(lambda: it_faker.sentences(nb=3))
+
+
+class EvaluationType(Enum):
+    STUDY_PLAN_ASSIGNMENT = "studyPlan"
+    SUBJECT_EVALUATION = "subjectEvaluation"
+
+
+@dataclass
+class Evaluation:
+    type_: EvaluationType = f(lambda: it_faker.random_element(elements=(elem.value for elem in EvaluationType)))
+    solved: List[Solves] = f(lst(Solves))
+    todo: List[Exercise] = f(lst(Exercise))
+
+
+# Evaluations and study plan assignment tests are same they exist on student
 # We generate and embed exercises on students.
 
+#### Events
+
+@dataclass
+class Event:
+    pass
+
+
+#### Study Plan
 
 @dataclass
 class Student:
     account_id: int = f(random_id)
     personal_info: PersonalInfo = f(PersonalInfo)
-    skill_set: List[Skill] = f(lambda: it_faker.random_choices(elements=all_skills, length=10))
-    evaluations: str = f(it_faker.text)
-    tutorial_status: str = f(it_faker.text)
-    exercise_attempts: str = f(it_faker.text)
+    skill_set: List[Skill] = f(lambda: it_faker.random_elements(elements=all_skills, length=10))
+    evaluations: Evaluation = f(lst(Evaluation))
+    tutorial_status: str = "done"
+    exercise_attempts: List[Solves] = f(lst(Solves))
     events: str = f(it_faker.text)
     study_plan: str = f(it_faker.text)
 
 
-elems = [asdict(Student()) for _ in range(5)]
-with open("test.json", "w") as file:
-    json.dump(elems, file)
+from pymongo import MongoClient
+from os import getenv
+username = getenv("MONGO_USER", "root")
+passwd = getenv("MONGO_PASS", "example")
+db_name = getenv("MONGO_DB", "test")
+coll_name = getenv("MONGO_COLL", "test")
+student_count = getenv("STUDENT_COUNT", "5")
+host = getenv("MONGO_HOST", "0.0.0.0")
+client = MongoClient(f"mongodb://{username}:{passwd}@{host}:27017/")
+db = client.get_database(db_name)
+db.drop_collection(coll_name)
+coll = db.get_collection(coll_name)
+elems = [asdict(Student()) for _ in range(int(student_count))]
+coll.insert_many(elems)
