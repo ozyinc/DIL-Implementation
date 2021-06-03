d18c90db629a4ec6faa11b84a13d8697a396c46a
aida <aida@csuitecircle.com>
2021-06-03 14:18:30 +0200

    added functions for submitting test

--- side/functions/index.js
+++ side/functions/index.js
@@ -4,7 +4,46 @@ const vision = require('@google-cloud/vision');
 
 const cors = require('cors')({origin: true});
 
+const analyseMultipleChoice = function(answer, correct) {
+  return correct == answer;
+}
+const analyseWriting = async function(answer, correct) {
+  functions.logger.info("received image to analyse");
+  // let result = '';
 
+  //base64 image: https://forum.freecodecamp.org/t/unable-to-detect-text-in-a-base64-image-uri-in-google-cloud-vision/421650
+
+  //Call Google Vision API
+  const client = new vision.ImageAnnotatorClient();
+
+  const fileName = './test.png';
+
+  const image = answer;
+
+  const imageDetectionReq = image ? {
+      image: {
+        content: Buffer.from(image, 'base64')
+      }
+  } : fileName;
+
+  
+  // Read a local image as a text document
+  const [result] = await client.documentTextDetection(imageDetectionReq);
+  const fullTextAnnotation = result.fullTextAnnotation;
+
+  console.log(`Full text: ${fullTextAnnotation.text}`);
+  let residue = fullTextAnnotation.text.trim().toLowerCase().replace(correct.toLowerCase(), '');
+
+  if(residue.length > 0) {
+    for(let c of residue) {
+      if(c >= 'a' && c<='z') {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -70,4 +109,53 @@ exports.evaluateWriting = functions.runWith({
         
     });
     
-})
+});
+
+exports.submitLevelAssessment = functions.runWith({
+  // Ensure the function has enough memory and time
+  // to process large files
+  timeoutSeconds: 300,
+  memory: "128MB",
+}).https.onRequest(async (request, response) => {
+  return cors(request, response, async () => {
+     
+    //structure: [{questionID:, questionType, correctAnswer, userAnswer:}]
+    let questions = request.body;
+
+    let results={
+      level: 0,
+      results: []
+    }
+
+    let countCorrect = 0;
+
+    for(let i=0; i<questions.length; i++) {
+      let result;
+      switch(questions[i].type) {
+        case 'MultipleChoice':
+          result = analyseMultipleChoice(questions[i].answer, questions[i].correctAnswer)
+          results.results.push({
+            questionID: questions[i].questionID,
+            result: result
+          });
+          break;
+        case 'Handwriting': 
+        result = await analyseWriting(questions[i].answer, questions[i].correctAnswer)
+        results.results.push({
+          questionID: questions[i].questionID,
+          result: result
+        });
+        break;
+        default:
+          break;
+      }
+      if(result) countCorrect++;
+    }
+
+    results.level = Math.floor( (countCorrect/results.results.length) * 5 + 1 )
+
+    response.json(results)
+      
+  });
+  
+});
